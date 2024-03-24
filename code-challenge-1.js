4d73d7e33ca3e8804fbd84b7280cd5bae8e907f0
TITAN <aluochmarion7@gmail.com>
2024-03-24 10:58:06 +0300

    Grading System

--- code-challenge-1.js
+++ code-challenge-1.js
@@ -1,18 +1,22 @@
 function studentGrade(grade){
-   
-    if (grade >=79){
-        return 'A'
-    }
-    else if (grade => 60 && grade<= 78){
-        return "B"
-    }
-    else if (grade =>50 && grade <= 59){
-        return "C"
-    }
-    else if (grade =>50 && grade <=49){
-        return "D"
-    }
-    else {
-        return "E"
-    }
-}
+  
+    const studentName= "Marion"
+   const studentMark = 0
+ 
+     if (grade >=79){
+         return 'A'
+     }
+     else if (grade >= 60 && grade<=79){
+         return "B"
+     }
+     else if (grade >=50 && grade <= 59){
+         return "C"
+     }
+     else if (grade >=50 && grade <=49){
+         return "D"
+     }
+     else {
+         return "E"
+     }
+ }
+ console.log(studentGrade(42))
